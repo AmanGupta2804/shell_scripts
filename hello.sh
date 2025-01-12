18d04a86988214469587665a881cc13e05e68598
Aman Gupta <55246803+AmanGupta2804@users.noreply.github.com>
2025-01-12 23:36:30 +0000

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+<<note this is hello
+file
+note
+
+echo "Gabbar: kitne aadmi the"
+
+echo "kalia : 2 addmi the sarkar"
+
+echo "kitna bja ha"
+date
+
+echo "kitne bje se chl rhi ha"
+uptime
